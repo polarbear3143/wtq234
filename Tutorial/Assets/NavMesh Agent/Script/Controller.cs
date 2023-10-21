8c016ecc46d20e8bc23648184137100141e41d82
cafemocha22 <144551556+cafemocha22@users.noreply.github.com>
2023-10-21 17:28:55 +0900

    (10.21)네비 메쉬 에이전
    ㅇㅇ

---
+++ Tutorial/Assets/NavMesh Agent/Script/Controller.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Controller : MonoBehaviour
+{
+
+    public int count;
+    public Transform[] point;
+    public NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        InvokeRepeating("Move", 1, 5f);      
+    }
+
+
+    public void Move()
+    {
+        if(navMeshAgent.velocity == Vector3.zero)
+        {
+            if (point.Length <= count)
+                count = 0;
+        }
+
+        navMeshAgent.SetDestination(point[count++].position);
+    }
+}
